16de1507cc07cf8df2fbb6e1af5414225b92bb05
Jan Moxter <jan.moxter@innobix.com>
2018-06-30 14:44:51 -0500

    Changing to different parser

--- src/main/java/org/efaps/tests/program/ESJPValidation.java
+++ src/main/java/org/efaps/tests/program/ESJPValidation.java
@@ -1,18 +1,15 @@
 package org.efaps.tests.program;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.io.FilenameUtils;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -34,48 +31,25 @@ public class ESJPValidation
                                  final File _file)
         throws IOException
     {
-        final File xmlFile = new File(_context.getCurrentXmlTest().getSuite().getFileName());
         final String application = _context.getCurrentXmlTest().getParameter("application");
-        final String baseFolderRel = _context.getCurrentXmlTest().getParameter("baseFolder");
-        final String baseFolder = FilenameUtils.concat(xmlFile.getPath(), baseFolderRel);
 
         final String str = Generic4VarArg.readFileToString(_file);
-        final ASTParser parser = ASTParser.newParser(AST.JLS8);
-
-        parser.setKind(ASTParser.K_COMPILATION_UNIT);
-        parser.setResolveBindings(true);
-        parser.setBindingsRecovery(true);
-        @SuppressWarnings("unchecked")
-        final Map<String, String> options = JavaCore.getOptions();
-        options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
-        parser.setCompilerOptions(options);
-        parser.setUnitName("any_name");
-        // _context.getClass().getClassLoader().
-        final String strClassPath = System.getProperty("java.class.path");
-        final String[] sources = { baseFolder + "/ESJP/" };
-        final String[] classpath = strClassPath.split(":");
-
-        parser.setEnvironment(classpath, sources, new String[] { "UTF-8" }, true);
-        parser.setSource(str.toCharArray());
-
         final Set<String> annotations = new HashSet<>();
-        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-        cu.accept(new ASTVisitor()
-        {
-
+        final CompilationUnit parsed = JavaParser.parse(str);
+        parsed.accept(new VoidVisitorAdapter<Void>() {
             @Override
-            public boolean visit(final SingleMemberAnnotation _node)
+            public void visit(final ClassOrInterfaceDeclaration _n, final Void _arg)
             {
-                annotations.add(_node.getTypeName().toString());
-                annotations.add(_node.toString());
-                return super.visit(_node);
+                _n.getAnnotations().forEach(an -> {
+                    annotations.add(an.getNameAsString());
+                    annotations.add(an.toString());
+                });
             }
-        });
+        }, null);
         LOG.debug("Annotations found {}", annotations);
         Assert.assertEquals(annotations.contains("EFapsUUID"), true,
                         String.format("ESJP '%s' does not contain the EFapsUUID annotation", _file));
         Assert.assertEquals(annotations.contains("@EFapsApplication(\"" + application + "\")"), true,
                         String.format("ESJP '%s' does not contain correect EFapsApplication annotation", _file));
-
     }
 }
